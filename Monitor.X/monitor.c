2ff0c544bf9983cc37225be8cccd4531121fc819
carlospardovidal <carlospardovidal@hotmail.com>
2020-04-08 23:07:15 +0200

    Added sampling filter. Slow and Fast

--- Monitor.X/monitor.c
+++ Monitor.X/monitor.c
@@ -18,24 +18,46 @@ int timeUp;
 #define STATE_OFF           0
 #define STATE_CALIBRATE     1
 #define STATE_LOW           2
+#define STATE_TDI           3
 #define STATE_HIGH          4
+#define STATE_TDE           5
 
 int state;
 
 uint32_t tt1, tt2, tt3, tt4, tdi, tde;
 
+
 uint32_t GetTde(void) { return tde; }
 uint32_t GetTdi(void) { return tdi; }
 int GetMonitorState(void) { return state; }
+uint8_t prSlowBuffer[25];
+uint8_t prFastBuffer[25];
+uint16_t count;
+uint8_t prSlow;
+uint8_t prFast;
+
 
 void MonitorPressureTask(void) {
     uint8_t pr;
     int next = state;
     uint32_t tt;
-    
+    uint16_t temp;
     
     pr = GetPressure_mbar();
     tt = tick_get_slow();
+    count ++;
+    prFastBuffer[count%25]=pr;
+    temp = 0;
+    for (int i=0; i<25; i++) temp +=prSlowBuffer[i];
+    prFast = temp/25;
+    if (count%40==0) {
+        prSlowBuffer[count/40]=pr;
+        temp = 0;
+        for (int i=0; i<25; i++) temp +=prSlowBuffer[i];
+        prSlow = temp/25;
+    }
+    if (count==40*25*25) count =0;
+    
     
     switch (state) {
         case STATE_OFF:
@@ -45,17 +67,27 @@ void MonitorPressureTask(void) {
         case STATE_CALIBRATE:
             break;
         case STATE_LOW:
-            if (pr<targetLow)  tt1 = tt;
-            if (pr>targetHigh) {
+            tt1 = tt;
+            if (prFast>prSlow) {
+                next = STATE_TDI;
+            }
+            break;
+        case STATE_TDI:
+            if (prFast<=prSlow) {
                 tt2 = tt;
                 next = STATE_HIGH;
                 tdi = tt2-tt1;
-                printf("\r\nTDI: %lu\r\n", tdi);
+                printf("\r\nTDI: %lu\r\n", tdi);    
             }
             break;
         case STATE_HIGH:
-            if (pr>targetHigh)  tt3 = tt;
-            if (pr<targetLow) {
+            tt3 = tt;
+            if (prFast<prSlow) {
+                next = STATE_TDE;
+            }
+            break;
+        case STATE_TDE:
+            if (prFast>=prSlow) {
                 tt4 = tt;
                 next = STATE_LOW;
                 tde = tt4-tt3;
@@ -77,6 +109,7 @@ void InitializePressure (void) {
     targetLow  = 10; // mbar
     adcOffset = 0; // Read from EEPROM or calibrate. ADC counts
     state = STATE_OFF;
+    count =0;
 }
 
 
